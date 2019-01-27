b71305eae481c85e28779b091637910d8e5f0d9b
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-01-27 23:12:50 +0100

    Update configure.sh

--- _script/configure.sh
+++ _script/configure.sh
@@ -7,7 +7,7 @@
 [ -z "$wallet" ] &&  exit 100
 [ -z "$currency" ] &&  exit 101
 
-cat <<EOF > runcpuminer-opt-yespower.sh
+cat <<EOF > ~/runcpuminer-opt-yespower.sh
 #!/usr/bin/env bash
 cd ~/cpuminer-opt-yespower && screen -dmS $currency.yespower ./cpuminer -a yespower -o stratum+tcp://mine.nlpool.nl:6234 -u $wallet -p c=$currency -q
 
