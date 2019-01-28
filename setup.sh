af9efbfa9912350845c48610d9b8d704851e2ee4
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-01-28 17:38:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 sudo apt-get -y update       
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git
