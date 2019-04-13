238f740f1ac5c957c9830e374f8d102a6435845f
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-04-13 15:15:14 +0200

    Update

--- _script/configure.sh
+++ _script/configure.sh
@@ -9,6 +9,6 @@
 
 cat <<EOF > ~/runcpuminer-opt-yespowerr16.sh
 #!/usr/bin/env bash
-cd ~/cpuminer-opt-yespower && screen -dmS $currency.yespowerr16 ./cpuminer -a yespowerr16 -o stratum+tcp://mine.nlpool.nl:6244 -u $wallet -p c=$currency -q
+cd ~/cpuminer-opt-yespower && screen -dmS $currency.yespowerR16 ./cpuminer -a yespowerR16 -o stratum+tcp://mine.nlpool.nl:6244 -u $wallet -p c=$currency -q
 
 EOF
