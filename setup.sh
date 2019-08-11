4d4ceefe28db6169505f8e7b5b11e9253d3ec548
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-08-11 20:39:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git make
 sudo apt -y install libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
 
-git clone -b yescryptzergpool https://github.com/ttoonka/yespowerminerbuild
+git clone -b yespowerzergpool https://github.com/ttoonka/yespowerminerbuild
 
 cd yespowerminerbuild
 chmod +x _script/compile.sh
