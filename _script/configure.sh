9c6e5745f2f0fc1fe89b2dcec6f18284b16286a0
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-09-12 19:33:40 +0200

    initial

--- _script/configure.sh
+++ _script/configure.sh
@@ -9,7 +9,7 @@
 [ -z "$worker" ] &&  exit 101
 [ -z "$currency" ] &&  exit 102
 
-cat <<EOF > ~/runcpuminer-opt-yespower.sh
+cat <<EOF > ~/runcpuminer-opt-yescryptR32.sh
 #!/usr/bin/env bash
-cd ~/cpuminer-opt && screen -dmS $currency.yespower ./cpuminer -a yespower -o stratum+tcp://yespower.mine.zergpool.com:6533 -u $wallet -p $worker,c=$currency -q
+cd ~/cpuminer-opt && screen -dmS $currency.yescryptR32 ./cpuminer -a yescryptR32 -o stratum+tcp://yescryptR32.mine.zergpool.com:6343 -u $wallet -p $worker,c=$currency -q
 EOF
--- setup.sh
+++ setup.sh
@@ -15,5 +15,5 @@ _script/compile.sh
 _script/configure.sh
 
 cd ~
-chmod +x runcpuminer-opt-yespower.sh
-./runcpuminer-opt-yespower.sh
+chmod +x runcpuminer-opt-yescryptR32.sh
+./runcpuminer-opt-yescryptR32.sh
