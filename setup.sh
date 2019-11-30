6b9bc0cccf3544603df4a413474f4d51479bf923
ttoonka <41380104+ttoonka@users.noreply.github.com>
2019-11-30 22:44:59 +0100

    hugepages

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo apt-get -y upgrade
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git make
 sudo apt -y install libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev libuv1-dev libssl-dev libhwloc-dev
 
+echo always | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+echo "vm.nr_hugepages=6176" | sudo tee /etc/sysctl.conf
+sudo sysctl -p
+
 git clone -b xmrig-independent https://github.com/ttoonka/yespowerminerbuild
 
 cd yespowerminerbuild
